5bcfacdb2b08572df839da5f2d6a2ad902e5a56f
Sanya <Sanya@DESKTOP-V0HNENC>
2016-11-08 17:52:45 +0200

    Change background image.

---
+++ MarsCitizens/MarsCitizens/Extensions/AspectRatioContainer.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace MarsCitizens.Extensions
+{
+    public class AspectRatioContainer : ContentView
+    {
+        public double AspectRatio { get; set; }
+
+        protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+        {
+            return new SizeRequest(new Size(widthConstraint, widthConstraint * AspectRatio));
+        }
+    }
+}
