301636f3d741836c0c6bceb5bba12a7a02b4462c
Sanya <Sanya@DESKTOP-V0HNENC>
2016-11-09 09:56:07 +0200

    Add CitizensPage.

---
+++ MarsCitizens/MarsCitizens/ViewModels/CitizensViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MarsCitizens.ViewModels
+{
+    public class CitizensViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
--- MarsCitizens/MarsCitizens/Views/MainPage.xaml.cs
+++ MarsCitizens/MarsCitizens/Views/MainPage.xaml.cs
@@ -1,16 +1,21 @@
-﻿using MarsCitizens.ViewModels;
+﻿using System;
 using Xamarin.Forms;
+using MarsCitizens.ViewModels;
 
 namespace MarsCitizens.Views
 {
     public partial class MainPage : ContentPage
     {
+        private TapGestureRecognizer _tapGestureRecognizer;
         private MainViewModel _viewModel;
+
         public MainPage()
         {
             InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
 
             _viewModel = BindingContext as MainViewModel;
+            _tapGestureRecognizer = new TapGestureRecognizer();
         }
 
         protected override async void OnAppearing()
@@ -19,6 +24,21 @@ namespace MarsCitizens.Views
 
             if (result.IsFailure)
                 await DisplayAlert("Mars Citizens", result.Error, "Close");
+
+            _tapGestureRecognizer.Tapped += OnContentViewTapped;
+            _contentView.GestureRecognizers.Add(_tapGestureRecognizer);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            _tapGestureRecognizer.Tapped -= OnContentViewTapped;
+            _contentView.GestureRecognizers.Remove(_tapGestureRecognizer);
+        }
+
+        private async void OnContentViewTapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CitiziensPage());
         }
     }
 }
